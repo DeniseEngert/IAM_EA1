05dc7957dced128846273245a404644d3d6db158
deniseengert <engertden@gmail.com>
2018-10-29 18:07:20 +0100

    Transition of views integrated

--- DoSomething.js
+++ DoSomething.js
@@ -2,19 +2,25 @@ function initialiseView() {
 
     var toggle = document.getElementById("toggleView");
     var body = document.body;
-    var refresh = document.querySelector(".refresh");
+    //var refresh = document.querySelector(".refresh");
     var main = document.querySelector("main");
 
+    // Toggle Views and Transitions
+
     toggle.onclick = () => {
+        main.classList.toggle("faded");
+        main.addEventListener("transitionend", onTransitionEnd);
+    };
+
+    function onTransitionEnd() {
+        main.classList.toggle("faded");
+        main.removeEventListener("transitionend", onTransitionEnd);
+
         if (body.classList.contains("tiles")) {
             body.classList.remove("tiles");
         } else {
             body.classList.add("tiles");
         }
 
-    };
-
-    refresh.onclick = () => {
-        main.classList.toggle("faded");
     }
 }
