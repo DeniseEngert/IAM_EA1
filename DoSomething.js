aefb3299a2b2931e0232f2db3b4241883848217a
deniseengert <engertden@gmail.com>
2018-10-30 23:41:45 +0100

    Integrated List Item Selection

--- DoSomething.js
+++ DoSomething.js
@@ -1,11 +1,10 @@
 function initialiseView() {
 
+    // === Toggle Views and Transitions ===
+
     var toggle = document.getElementById("toggleView");
     var body = document.body;
-    //var refresh = document.querySelector(".refresh");
     var main = document.querySelector("main");
-
-    // Toggle Views and Transitions
 
     toggle.onclick = () => {
         main.classList.toggle("faded");
@@ -22,5 +21,48 @@ function initialiseView() {
             body.classList.add("tiles");
         }
 
+    }
+
+    // === Auswahl Listenelement ===
+
+    var ul = document.getElementsByTagName("UL")[0];
+
+    function lookUpLi(el){
+        if (el.tagName === "LI"){
+            console.log("LI");
+            return el;
+        } else if (el.className === "option") {
+            console.log("option");
+            return el;
+        } else if (el.tagName === "UL"){
+            console.error("lookUpLi(): reached list root");
+            return null;
+        } else if (el.parentNode) {
+            return lookUpLi(el.parentNode);
+        } else {
+            console.error("lookUpLi(): something went wrong, get: ", el);
+            return null;
+        }
+    }
+
+    ul.onclick = (evt) => {
+        var selectedLI = lookUpLi(evt.target);
+        alert("selected: " + getListitemName(selectedLI));
+        //;
+    };
+
+    function getListitemName(li) {
+        return li.querySelector("h2").textContent;
+    }
+
+    // === Listenelement hinzufuegen ===
+
+    const add = document.querySelector("header .add");
+    add.onclick = () => {
+        addNewLi({title: "Title", src: "img/300_100.jpeg"})
+    };
+
+    function addNewLi(obj) {
+        alert("addNewLi(): " + JSON.stringify(obj));
     }
 }
